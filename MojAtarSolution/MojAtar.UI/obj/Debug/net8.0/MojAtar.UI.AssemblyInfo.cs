36b1a729e7b3287a4e8d4d67a1c66dbd37fc9d1b
Faramir175 <faramilan1@gmail.com>
2025-04-08 19:08:57 +0200

    U update se ne moze promeniti tip radnje

--- MojAtarSolution/MojAtar.UI/obj/Debug/net8.0/MojAtar.UI.AssemblyInfo.cs
+++ MojAtarSolution/MojAtar.UI/obj/Debug/net8.0/MojAtar.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MojAtar.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d940a4ce6560143d46d8536155bd53397b60752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d656d4d00526a11483a393731d6cedfc0490bf56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MojAtar.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MojAtar.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
